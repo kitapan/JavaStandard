6aafe0d76db03a459714453064a14889644a1e18
kita <kitagawa.hisashi@link-academy.co.jp>
2024-07-15 07:08:37 +0900

    FirstCommit

---
+++ src/main/java/raisetech/StudentManagement/repository/StudentRepository.java
@@ -0,0 +1,79 @@
+package raisetech.StudentManagement.repository;
+
+import jakarta.validation.constraints.Pattern;
+import java.util.List;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import raisetech.StudentManagement.data.Student;
+import raisetech.StudentManagement.data.StudentCourse;
+
+/**
+ * 受講生テーブルと受講生コース情報テーブルと紐づくRepositoryです。
+ */
+
+@Mapper
+public interface StudentRepository {
+
+  /**
+   * 受講医の全件検索を行います。
+   *
+   * @return 受講生一覧（全件）
+   */
+  List<Student> search();
+
+  /**
+   * 受講生の検索を行います。
+   *
+   * @param id 受講生ID
+   * @return 受講生
+   */
+  Student searchStudent(String id);
+
+  /**
+   * 受講生のコース情報の全件検索を行います。
+   *
+   * @return 受講生のコース情報（全件）
+   */
+  List<StudentCourse> searchStudentCourseList();
+
+  /**
+   * 受講生IDに紐づく受講生コース情報を検索します。
+   *
+   * @param studentId 受講生ID
+   * @return 受講生IDに紐づく受講生コース情報
+   */
+  List<StudentCourse> searchStudentCourse(@Param("studentId") String studentId);
+
+  /**
+   * 受講生を新規登録します。 IDに関しては自動採番を行う。
+   *
+   * @param student 受講生
+   */
+  void registerStudent(@Param("student") Student student);
+
+  /**
+   * 受講生コース情報を新規登録します。IDに関しては自動採番を行う。
+   *
+   * @param studentCourse 受講生コース情報
+   */
+  void registerStudentCourse(@Param("studentCourse") StudentCourse studentCourse);
+
+  /**
+   * 受講生を更新します。
+   *
+   * @param student 受講生
+   */
+  void updateStudent(@Param("student") Student student);
+
+  /**
+   * 受講生コース情報のコース名を更新します。
+   *
+   * @param studentCourse 受講生コース情報
+   */
+  void updateStudentCourse(@Param("studentCourse") StudentCourse studentCourse);
+
+}
