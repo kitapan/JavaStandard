544236e3884daea98b78646dcff7633c169c40d5
kita <kitagawa.hisashi@link-academy.co.jp>
2024-07-15 07:08:37 +0900

    FirstCommit

---
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -0,0 +1,105 @@
+package raisetech.StudentManagement.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import raisetech.StudentManagement.data.StudentCourse;
+import raisetech.StudentManagement.domain.StudentDetail;
+import raisetech.StudentManagement.exception.TestException;
+import raisetech.StudentManagement.service.StudentService;
+
+/**
+ * 受講生の検索や登録、更新などを行うREST APIとして実行されるControllerです。
+ */
+@Validated
+@RestController
+public class StudentController {
+
+  private StudentService service;
+
+  @Autowired
+  public StudentController(StudentService service) {
+    this.service = service;
+  }
+
+  /**
+   * 受講生詳細の一覧検索です。 全件検索を行うので、条件指定は行いません。
+   *
+   * @return 受講生詳細一覧（全件）
+   */
+  @Operation(summary = "一覧検索", description = "受講生の一覧を検索します。")
+  @GetMapping("/studentList")
+  public List<StudentDetail> getStudentList() throws TestException {
+    throw new TestException(
+        "現在のこのAPIは利用できません。URLは『studentList』ではなく「students」を利用してください。");
+  }
+
+  /**
+   * 受講生詳細の検索です。 IDに紐づく任意の受講生の情報を取得します。
+   *
+   * @param id 受講生ID
+   * @return 受講生
+   */
+
+  @GetMapping("/student/{id}")
+  public StudentDetail getStudent(
+      @PathVariable @NotBlank @Pattern(regexp = "^\\d+$") String id) {
+    return service.searchStudent(id);
+  }
+
+  @GetMapping("/newStudent")
+  public String newStudent(Model model) {
+    StudentDetail studentDetail = new StudentDetail();
+    studentDetail.setStudentCourseList(Arrays.asList(new StudentCourse()));
+    model.addAttribute("studentDetail", studentDetail);
+    return "registerStudent";
+  }
+
+  /**
+   * 受講生詳細の登録を行います。
+   *
+   * @param studentDetail 受講生詳細
+   * @return 実行結果
+   */
+
+  @PostMapping("/registerStudent")
+  public ResponseEntity<StudentDetail> registerStudent(
+      @RequestBody @Valid StudentDetail studentDetail) {
+    StudentDetail responseStudentDetail = service.registerStudent(studentDetail);
+    return ResponseEntity.ok(responseStudentDetail);
+  }
+
+  /**
+   * 受講生詳細の更新を行います。 キャンセルフラグの更新もここで行います（論理削除）
+   *
+   * @param studentDetail 受講生詳細
+   * @return 実行結果
+   */
+  @Operation(summary = "受講生登録", description = "受講生を登録します。")
+  @PutMapping("/updateStudent")
+  public ResponseEntity<String> updateStudent(@RequestBody @Valid StudentDetail studentDetail) {
+    service.updateStudent(studentDetail);
+    return ResponseEntity.ok("更新処理が成功しました。");
+  }
+
+  @ExceptionHandler(TestException.class)
+  public ResponseEntity<String> handleTestException(TestException ex){
+    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+  }
+}
